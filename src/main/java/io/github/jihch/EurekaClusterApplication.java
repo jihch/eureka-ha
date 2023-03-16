6e92a7323b09fd6015f0c463f71a9e2b0921b6ce
jihch <haoqibaobaoji@126.com>
2023-03-16 09:51:40 +0800

    +1

---
+++ src/main/java/io/github/jihch/EurekaClusterApplication.java
@@ -0,0 +1,17 @@
+package io.github.jihch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaClusterApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(EurekaClusterApplication.class, args);
+
+    }
+
+}
